0f3aebc0a38995a75ebefc1daa8384895140f8ff
Umer Razzaq <o.razzaq@yahoo.com>
2015-11-19 17:36:54 -0700

    DetailsModal
    latest

--- DetailsModal.php
+++ DetailsModal.php
@@ -17,16 +17,15 @@ if (!$brand_query) {
     }
 $brand = mysqli_fetch_assoc($brand_query);
 $sizestring = $product['sizes'];
-
-
+$size_array = explode(',',$sizestring);
 ?>
-
+<!--Start Buffer-->
 <?php ob_start(); ?>
 <div class = "modal fade details-1" id="details-modal" tabindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
     <div class = "modal-dialog modal-lg">
         <div class = "modal-content">
                 <div class = "modal-header">
-                    <button class = "close" type = "button" data-dismiss="modal" aria-label="Close">
+                    <button class = "close" type = "button" onclick="closeModal()" aria-label="Close">
                         <span aria-hidden= "true">&times;</span>
                     </button>
                     <h4 class = "modal-title text-center"><?php echo $product['title']; ?></h4>
@@ -40,7 +39,7 @@ $sizestring = $product['sizes'];
                                 </div>
                             </div>   
                             <div class= "col-sm-6">
-                                <h4>Details<h4>
+                                <h4>Details<h4>                  
                                 <p><?php echo $product['description']; ?></p>
                                 <hr>
                                 <p>Price: <?php echo $product['price']; ?></p>
@@ -48,11 +47,23 @@ $sizestring = $product['sizes'];
                                 <form action="add_cart.php" method ="post">
                                     <div class = "form-group">
                                         <div class = "col-xs-3">
-                                            <label for="quantity">Quantity:</label>
+                                            <label for="quantity">Quantity:</label>                                        
                                             <input type="text" class = "form-control" id="quantity" name="quantity">
-                                            
+                                        <div class ="form-group">
+                                            <label for='size'>Size</label>
+                                            <select name ="size" id ="size" class ="form-control">
+                                                <option value = ""></option>
+                                                <?php foreach($size_array as $string){
+                                                    $string_array = explode(':',$string);
+                                                    $size = $string_array[0];
+                                                    $quantity = $string_array[1];
+                                                    echo '<option value = "'.$size.'">'.$size.' ('.$quantity.' Available)</option>' ;    
+                                                } ?>
+                                               
+                                            </select>
                                         </div>
                                         <p>1</p>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
@@ -60,11 +71,24 @@ $sizestring = $product['sizes'];
                     </div>	
                 </div>
                 <div class ="modal-footer">
-                    <button class = "btn btn-default" data-dismiss="modal">Close</button>
+                    <button class = "btn btn-default" onclick="closeModal()">Close</button>
                     <button class = "btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"></span>Add to Cart</button>
                 </div>
         </div>	
     </div>
 </div>
+<!--Close modal function-->
+<script>
+    function closeModal(){
+        jQuery('#details-modal').modal('hide');
+        setTimeout(function(){
+            jQuery('#details-modal').remove();
+            jQuery('.modal-backdrop').remove();
+        },500);
+    }
+    
+</script>
+    
 
 <?php echo ob_get_clean(); ?>
+<!--End Buffer-->
